285b1c15e202f735d72277c970bb7c60bdd81f5a
Mohammad Ibrahim <moham64@outlook.com>
2018-02-16 03:12:53 -0500

    finished SetUsername.java

--- app/src/main/java/com/mibrh/firechat/SetUsername.java
+++ app/src/main/java/com/mibrh/firechat/SetUsername.java
@@ -1,8 +1,14 @@
 package com.mibrh.firechat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.id.message;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SetUsername extends AppCompatActivity {
 
@@ -14,6 +20,17 @@ public class SetUsername extends AppCompatActivity {
         setContentView(R.layout.activity_set_username);
 
         editText = (EditText) findViewById(R.id.editText1);
-
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (actionId == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    Intent intent = new Intent(this, MainActivity.class);
+                    intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
     }
 }
