594b7fde6cca5b3d74069ce5462242d1f5c66b6c
Mohammad Ibrahim <moham64@outlook.com>
2018-02-16 18:21:44 -0500

    refactor username activity and view

--- app/src/main/java/com/mibrh/firechat/SetUsername.java
+++ app/src/main/java/com/mibrh/firechat/SetUsername.java
@@ -6,11 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SetUsername extends AppCompatActivity {
-
     EditText editText;
 
 
@@ -19,21 +19,33 @@ public class SetUsername extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_username);
 
-        final Intent intent = new Intent(this, MainActivity.class);
-
-        editText = (EditText) findViewById(R.id.editText1);
+        editText = (EditText) findViewById(R.id.username_field);
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (keyCode == KeyEvent.KEYCODE_ENTER)
                     if (event.getAction() == KeyEvent.ACTION_DOWN){
                         // Perform action on key press
-                        intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
-                        startActivity(intent);
+                        usernameSelected(null);
                         return true;
                     }
                 return false;
             }
         });
+    }
+
+    public void usernameSelected(View view){
+        String username = editText.getText().toString();
+
+        // TODO:
+        // Validate username (i.e. check for whitespaces)
+
+        if (username.isEmpty()) {
+            Toast.makeText(this, "Username not valid", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
+            startActivity(intent);
+        }
     }
 }
